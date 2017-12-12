65cdae018c9e1a693265020958af915b283e58ae
xurenhe <xurenhe19910131@163.com>
2017-12-12 22:59:28 +0800

    优先使用JSON序列化，来实现功能

--- llama-core/src/main/java/com/wojustme/llama/core/util/serializer/JsonUtils.java
+++ llama-core/src/main/java/com/wojustme/llama/core/util/serializer/JsonUtils.java
@@ -3,12 +3,15 @@ package com.wojustme.llama.core.util.serializer;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import java.io.UnsupportedEncodingException;
+
 /**
  * @author xurenhe
  * @date 2017/11/29
  */
 public final class JsonUtils {
     private static final Gson gson = new GsonBuilder().create();
+    private static final String CHARSETNAME = "UTF-8";
 
     public static <T> String toJsonStr(T obj) throws SerializerException {
         String json;
@@ -26,6 +29,28 @@ public final class JsonUtils {
             pojo = gson.fromJson(json, type);
         } catch (Exception e) {
             throw new SerializerException(e);
+        }
+        return pojo;
+    }
+
+    public static <T> byte[] serializer(T obj) throws SerializerException {
+        byte[] bytes;
+        try {
+            String jsonStr = toJsonStr(obj);
+            bytes = jsonStr.getBytes(CHARSETNAME);
+        } catch (UnsupportedEncodingException e) {
+            throw new SerializerException("Unsupport UTF-8 encode", e);
+        }
+        return bytes;
+    }
+
+    public static <T> T deserializer(byte[] bytes, Class<T> clazz) throws SerializerException {
+        T pojo;
+        try {
+            String str = new String(bytes, CHARSETNAME);
+            pojo = toBeanObj(str, clazz);
+        } catch (UnsupportedEncodingException e) {
+            throw new SerializerException("Unsupport UTF-8 encode", e);
         }
         return pojo;
     }
